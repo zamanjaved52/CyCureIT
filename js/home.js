e4bc16d4da653c848e7f27d8909efd2a7434c485
zamanjaved52 <zamanjavd527@gmail.com>
2022-01-13 08:29:15 +0500

    add

---
+++ js/home.js
@@ -0,0 +1,10 @@
+/*navbar*/
+function openNav() {
+    document.getElementById("mySidenav_sc").style.width = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav_sc").style.width = "0";
+}
+
+/*/navbar*/
